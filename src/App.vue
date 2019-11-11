125c66c92097eded76a37192dc8fe4db7968bb61
xiaobai1035 <2415979887@qq.com>
2019-11-11 17:26:28 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,61 @@
 <template>
- <el-container>
-    <el-form @submit.native.prevent="saveArticle" :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2">
-        <el-row>
-          <el-col :span="12">
-            <el-input v-model="ruleForm2.username" placeholder="用户名/邮箱/电话" auto-complete="on" clearable></el-input>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="12">
-            <el-input type="password" v-model="ruleForm2.password" auto-complete="off" placeholder="密码"></el-input>
-          </el-col>
-        </el-row>
-        <el-form-item>
-            <el-button type="primary" native-type="submit">提交</el-button>
-            <el-button @click="resetForm('ruleForm2')">重置</el-button>
-        </el-form-item>
-    </el-form>
- </el-container>
+  <el-container id="indexPage">
+    <el-header style="height: 30vh;padding: 25vh 0 0 0;">
+      <h1>
+        Element ui test
+      </h1>
+    </el-header>
+    <el-main>
+      <el-row type="flex" justify="center">
+        <el-col :span="10">
+          <el-form @submit.native.prevent="submitForm" :model="userForm" status-icon ref="userForm">
+            <el-form-item prop="username">
+              <el-input v-model="userForm.username" auto-complete="on" placeholder="用户名/手机/邮箱" clearable></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input type="password" v-model="userForm.password" auto-complete="off" placeholder="密码" clearable></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" native-type="submit">提交</el-button>
+              <el-button @click="resetForm('userForm')">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
-  export default {
-    data() {
-      var checkAge = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('年龄不能为空'));
-        }
-        setTimeout(() => {
-          if (!Number.isInteger(value)) {
-            callback(new Error('请输入数字值'));
-          } else {
-            if (value < 18) {
-              callback(new Error('必须年满18岁'));
-            } else {
-              callback();
-            }
-          }
-        }, 1000);
-      };
-      var validatePass = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入密码'));
-        } else {
-          if (this.ruleForm2.checkPass !== '') {
-            this.$refs.ruleForm2.validateField('checkPass');
-          }
-          callback();
-        }
-      };
-      var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请再次输入密码'));
-        } else if (value !== this.ruleForm2.pass) {
-          callback(new Error('两次输入密码不一致!'));
-        } else {
-          callback();
-        }
-      };
-      return {
-        ruleForm2: {
-          pass: '',
-          checkPass: '',
-          age: ''
-        },
-        rules2: {
-          pass: [
-            { validator: validatePass, trigger: 'blur' }
-          ],
-          checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-          age: [
-            { validator: checkAge, trigger: 'blur' }
-          ]
-        }
-      };
+export default {
+  data() {
+    return {
+      userForm: {}
+    };
+  },
+  methods: {
+    submitForm() {
+      // 校验用户信息
+      this.$router.push('/home')
     },
-    methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
   }
+}
 </script>
+
+<style>
+  .el-container {
+    padding: 0;
+    margin: 0;
+  }
+  #indexPage {
+    text-align: center;
+  }
+  .el-form {
+    border: 1px solid #DCDFE6;
+    border-radius: 5px;
+    padding: 2vh;
+  }
+</style>
