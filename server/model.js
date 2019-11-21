9583606b9c0582f3633ce9ab60201e680dce32d9
xiaobai1035 <2415979887@qq.com>
2019-11-21 17:02:54 +0800

    更新模型

--- server/model.js
+++ server/model.js
@@ -2,17 +2,38 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/element-admin', {
     useNewUrlParser: true,
-    useFindAndModify: true,
+    // useFindAndModify: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 })
 
-const Article = mongoose.model('Article', new mongoose.Schema({
-    title: {type: String},
-    body: {type: String}
-}))
+// 分类
+const categorySchema = new mongoose.Schema({
+    category_name: {type: String}
+}, {timestamps: {createdAt: 'created', updatedAt: 'updated'}})
 
-const User = mongoose.model('User', new mongoose.Schema({
+// 评论
+const commentSchema = new mongoose.Schema({
+    user_id: {type: String},
+    article_id: {type: String},
+    comment_like_count: {type: Number},
+    comment_content: {type: String},
+    comment_time: {type: String}
+}, {timestamps: {createdAt: 'created', updatedAt: 'updated'}})
+
+// 博文
+const articleSchema = new mongoose.Schema({
+    user_id: {type: String},
+    title: {type: String},
+    body: {type: String},
+    article_category_id: {type: String},
+    article_view_count: {type: Number},
+    article_comment_count: {type: Number},
+    article_like_count: {type: Number}
+}, {timestamps: {createdAt: 'article_created_time', updatedAt: 'article_update_time'}})
+
+// 用户
+const userSchema = new mongoose.Schema({
     username: {type: String, unique: true},
     password: {type: String,
         set(val) {
@@ -21,7 +42,12 @@ const User = mongoose.model('User', new mongoose.Schema({
     },
     phone: {type: String},  // 电话号验证
     mail: {type: String}    // mail验证
-}))
+}, {timestamps: {createdAt: 'created', updatedAt: 'updated'}})
+
+const Comment = mongoose.model('Comment', commentSchema)
+const Category = mongoose.model('Category', categorySchema)
+const Article = mongoose.model('Article', articleSchema)
+const User = mongoose.model('User', userSchema)
 
 
-module.exports = {Article, User}
+module.exports = {Article, User, Comment, Category}
