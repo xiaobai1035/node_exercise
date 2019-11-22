36b89f963e684de3aa065c9a768918af00046782
xiaobai1035 <2415979887@qq.com>
2019-11-22 17:11:05 +0800

    Update ListArticle.vue

--- src/views/ListArticle.vue
+++ src/views/ListArticle.vue
@@ -1,45 +1,65 @@
 <template>
-    <div>
-        <el-table :data="articles">
-            <el-table-column prop="title" label="标题" width="140">
-            </el-table-column>
-            <el-table-column prop="body" label="内容" width="220">
-            </el-table-column>
-            <el-table-column label="操作">
-                <template slot-scope="scope">
-                    <el-button size="mini" @click="handleEdit(scope.row._id)">编辑</el-button>
-                    <el-button size="mini" type="danger" 
-                    @click="handleDelete(scope.row._id)">删除</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+    <div style="overflow:auto">
+        <ul>
+            <li v-for="item in articleList" :key='item._id' style="list-style-type:none;">
+                <el-row>
+                    <el-link :underline="false" style="color: black;" @click="handleDetail(item._id)">
+                        <h1><strong>
+                            {{ item.title }}
+                        </strong></h1>
+                    </el-link>
+                </el-row>
+                <el-row>
+                    <el-col :sm="12" :md="16">
+                        头像
+                    </el-col>
+                    <el-col :sm="12" :md="8" style="text-align: right;">
+                        <el-link :underline="false" icon="el-icon-view" v-if="item.article_view_count" type="info">
+                            <span style="color: #3399ea;">
+                                {{item.article_view_count}}
+                            </span>
+                        </el-link>
+                        <el-divider direction="vertical" class="font-dicider" v-if="item.article_comment_count"></el-divider>
+                        <el-link :underline="false" icon="el-icon-chat-dot-round" v-if="item.article_comment_count" type="info">
+                            <span style="color: #3399ea;">
+                                {{item.article_comment_count}}
+                            </span>
+                        </el-link>
+                        <el-divider direction="vertical" v-if="item.article_like_count"></el-divider>
+                        <el-link :underline="false" icon="el-icon-thumb" v-if="item.article_like_count" type="info">
+                            <span style="color: #3399ea;">
+                                {{item.article_like_count}}
+                            </span>
+                        </el-link>
+                    </el-col>
+                </el-row>
+                <el-divider></el-divider>
+            </li>
+        </ul>
     </div>
 </template>
+
+<style>
+    .el-divider--horizontal{
+        margin: 7px 0;
+    }
+</style>
 
 <script>
     export default {
         data() {
             return {
-                articles: [] 
+                articleList: []
             }
         },
         methods: {
             fetch(){
                 this.$http.get('articles').then(res => {
-                    this.articles = res.data;
+                    this.articleList = res.data;
                 })
             },
-            handleDelete(id) {
-                this.$http.delete(`articles/${id}`).then(res => {
-                    this.$message({
-                        message: res.data.msg,
-                        type: 'success'
-                    });
-                    this.fetch()
-                })
-            },
-            handleEdit(id) {
-                this.$router.push(`/articles/${id}/edit`)
+            handleDetail(id) {
+                this.$router.push(`/articles/${id}/detail`)
             }
         },
         created(){
