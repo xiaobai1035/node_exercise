4689415ea2017329d5767b00710f030bc2843d8c
xiaobai1035 <2415979887@qq.com>
2019-11-25 13:40:34 +0800

    Update CreateArticle.vue

--- src/views/CreateArticle.vue
+++ src/views/CreateArticle.vue
@@ -1,14 +1,19 @@
 <template>
     <el-form @submit.native.prevent="saveArticle" ref="form" :model="article" label-width="80px">
+        <el-form-item label="文章分类">
+          <el-select v-model="category_value" placeholder="请选择">
+            <el-option v-for="item in categorys" :key="item.value" :label="item.label" :value="item.value"></el-option>
+          </el-select>
+        </el-form-item>
         <el-form-item label="文章标题">
-            <el-input v-model="article.title"></el-input>
+          <el-input v-model="article.title"></el-input>
         </el-form-item>
         <el-form-item label="文章内容">
-            <el-input type="textarea" v-model="article.body"></el-input>
+          <el-input type="textarea" v-model="article.body"></el-input>
         </el-form-item>
         <el-form-item>
-            <el-button type="primary" native-type="submit">立即创建</el-button>
-            <el-button>取消</el-button>
+          <el-button type="primary" native-type="submit">立即创建</el-button>
+          <el-button>取消</el-button>
         </el-form-item>
     </el-form>
 </template>
@@ -19,11 +24,14 @@
       return {
         article: {
 
-        }
+        },
+        categorys: [],
+        category_value: ''
       }
     },
     methods: {
       saveArticle() {
+        this.article.article_category_id = this.category_value;
         this.$http.post('articles', this.article, {
           headers: {
             'content-type': 'application/json',
@@ -41,7 +49,17 @@
             }
             return false;
         });
+      },
+      fetch(){
+        this.$http.get('categorys').then(res => {
+          for (var item of res.data) {
+            this.categorys.push({value: item._id, label: item.category_name})
+          }
+        })
       }
+    },
+    created(){
+        this.fetch()
     }
   }
 </script>
